7f9dfeaba6f3f22d84958360580f7a1d479d5db6
Michael Mangino <mmangino@elevatedrails.com>
2008-08-06 09:34:38 -0400

    Update code to perform single report

--- starling_monitor/starling_monitor.rb
+++ starling_monitor/starling_monitor.rb
@@ -19,12 +19,15 @@ class StarlingMonitor < Scout::Plugin
     self.connection=Starling.new("#{option(:host)}:#{option(:port)}")
   end
   
+    
+  
   def build_report
     begin
       setup_starling
       connection.sizeof(:all).each do |queue_name,item_count|
         check_queue(queue_name,item_count) if should_check_queue?(queue_name)
       end
+      report(@report)
     rescue  MissingLibrary=>e
       error("Could not load all required libraries",
             "I failed to load the starling library. Please make sure it is installed.")
@@ -39,7 +42,8 @@ class StarlingMonitor < Scout::Plugin
   
   def check_queue(name,depth)
     q_depth = (depth||0).to_i
-    report(name => q_depth)
+    @report ||= {}
+    @report[name] = q_depth
     if q_depth > option(:max_depth).to_i
       alert("Max Queue Depth for #{name} exceeded","#{q_depth} items is more than the max allowed #{option(:max_depth)}")
     end
